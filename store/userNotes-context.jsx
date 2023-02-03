2dbc881b3975f2628e54a711dc1d7d39fa643f7d
Gael Guyon <gael.guyon@capgemini.com>
2023-02-03 18:01:19 +0100

    add, edit, remove into firebase + prepare for favorite functionality + change add button to FAB

--- store/userNotes-context.jsx
+++ store/userNotes-context.jsx
@@ -1,14 +1,28 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+import { getNotes } from "../api/notesApi";
 
 export const NotesContext = createContext({
   notes: [],
   addNote: (note) => {},
   editNote: (note) => {},
   deleteNote: (note) => {},
+  isFetching: false,
 });
 
 function NotesContextProvider({ children }) {
   const [notes, setNotes] = useState([]);
+  const [isFetchingNotes, setIsFetchingNotes] = useState(false);
+
+  useEffect(() => {
+    setIsFetchingNotes(true);
+    async function fetchNotes() {
+      const data = await getNotes();
+      setNotes(data);
+      setIsFetchingNotes(false);
+    }
+    fetchNotes();
+  }, []);
+
   function addNote(note) {
     setNotes((currentNotes) => [note, ...currentNotes]);
   }
@@ -30,7 +44,13 @@ function NotesContextProvider({ children }) {
     );
   }
 
-  const value = { notes, addNote, deleteNote, editNote };
+  const value = {
+    notes,
+    addNote,
+    deleteNote,
+    editNote,
+    isFetching: isFetchingNotes,
+  };
 
   return (
     <NotesContext.Provider value={value}>{children}</NotesContext.Provider>
